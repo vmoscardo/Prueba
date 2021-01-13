1f91df779799e0df1dbeea18352cf6cfa58898ad
Vicente Moscardó <vicentemoscardo@hotmail.es>
2021-01-13 11:56:31 +0100

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Vicente Moscardo Gil on 13/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
